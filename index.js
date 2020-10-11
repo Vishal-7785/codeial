c9c57434d314d43694a0bc67c636b3aef750f85d
Vishal <vishaljaiswal7785@gmail.com>
2020-10-12 01:28:13 +0530

    We added partials and layouts

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 8005;
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+// extract styles and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+app.use(express.static('./assets'));
+// set up express router
+app.use('/',require('./routes'));
 // set up view engine
  app.set('view engine','ejs');
  app.set('views','./views');
-app.use('/',require('./routes'));
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
