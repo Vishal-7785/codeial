2a27053c5e96d2322cf4caf9491dbc567c78f8f1
Vishal <vishaljaiswal7785@gmail.com>
2020-10-14 19:31:17 +0530

    Created signout for passport.js

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,8 @@ router.get('/profile', passport.checkAuthentication,usersController.profile);
 router.get('/signup',usersController.signup);
 router.get('/signin',usersController.signin);
 router.post('/create',usersController.create)
+// For sign out
+router.get('/sign-out',usersController.destroySession);
 // Using passport as a middleware to authenticate
 router.post('/createSession',passport.authenticate(
     // This local tells that we have used local authentiation
